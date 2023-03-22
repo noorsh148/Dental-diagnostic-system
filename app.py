10931423c00b4c0a65fe8eeedb628a002eb4cd10
mohamd-johmani@ <mohamdaljohmani@gmail.com>
2023-03-22 20:37:05 +0200

    add standard TEETH_PREDICTION function

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask , render_template
-
+from flask import Flask , render_template,request,jsonify
+import os
+import werkzeug
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
+
+
+@app.route('/DentalAPI',methods=["TEETH_PREDICTION"])
+def upload():
+    if(request.method == "UPLOADIMAGE"):
+        imagefile = request.files['image']
+        filename = werkzeug.utils.secure_filename(imagefile.filename)
+        imagefile.save("./X-Ray-image/" + filename)
+        #TODO prediction = CoolStuff(imageFile)
+        os.remove("./X-Ray-image/" + filename);
+        # return jsonify({"predictionResult":prediction})
+
+
 if __name__ == "__main__":
-    app.run()   
+    app.run(debug=True,port=5000)
